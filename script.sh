80400a0388ff00c5f79a1a3758de0aedf2286d83
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2018-02-06 13:47:52 +0200

    Test V2

--- script.sh
+++ script.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
-# Export Variables
-export romsite="http://bigota.d.miui.com"
-extract="create_flashable_firmware.sh"
-upload='curl --upload-file'
-export miuidate=$(curl -s http://en.miui.com/forum.php | grep http://en.miui.com/download.html | cut -d ">" -f 3 | cut -d '<' -f 1 | tee) && echo "Latest miui update is $miuidate"
-
-# Fetch Updates
-echo "capricorn: Global"
-capricorn_global=$(curl -s http://en.miui.com/download-314.html | grep $romsite"/"$miuidate"/"miui_MI5SGlobal_$miuidate_)
-if [ "$capricorn_global" != "" ] ; then
-export MI5SGlobal=$(echo $capricorn_global | cut -d "/" -f 5 | cut -d '"' -f 1 | tee )
-echo File Name= $MI5SGlobal && echo Url= $romsite"/"$miuidate"/"$MI5SGlobal
-fi
-echo "capricorn: China"
-capricorn_china=$(curl -s http://en.miui.com/download-314.html#456 | grep $romsite"/"$miuidate"/"miui_MI5S_$miuidate_)
-if [ "$capricorn_china" != "" ] ; then
-export MI5S=$(echo $capricorn_china | cut -d "/" -f 5 | cut -d '"' -f 1 | tee )
-echo File Name= $MI5S && echo Url= $romsite"/"$miuidate"/"$MI5S
-fi
-
-# Downloading
-wget -qq --progress=bar https://github.com/xiaomi-firmware-updater/xiaomi-flashable-firmware-creator/raw/master/create_flashable_firmware.sh && chmod +x create_flashable_firmware.sh
-echo "Working on capricorn_global"
-wget -qq --progress=bar $romsite"/"$miuidate"/"$MI5SGlobal && ./$extract $MI5SGlobal && rm $MI5SGlobal
-echo "Working on capricorn_china"
-wget -qq --progress=bar $romsite"/"$miuidate"/"$MI5S && ./$extract $MI5S && rm $MI5S
-
-#Uploading
+echo Exporting variables:
+miuidate=$(curl -s http://en.miui.com/forum.php | grep http://en.miui.com/download.html | grep -o '[0-9]*[.][0-9]*[.][0-9]*') && echo "Latest miui update is $miuidate"
+site=http://bigota.d.miui.com/$miuidate/
+echo Fetching updates:
+while read device; do
+id=$(echo $device | cut -d , -f1)
+name=$(echo $device | cut -d , -f2)
+{
+echo Device: $(cut -d _ -f1 <<< $name) ; echo id: $id
+curl -s http://en.miui.com/download-$id.html | grep miui_$name$miuidate | cut -d '"' -f6 | cut -d '"' -f1
+} >> data
+done <devices
+cat data | grep -E "(http|https)://[a-zA-Z0-9./?=-]*" > links
+echo Downloading:
+wget --progress=bar -qq https://github.com/xiaomi-firmware-updater/xiaomi-flashable-firmware-creator/raw/master/create_flashable_firmware.sh && chmod +x create_flashable_firmware.sh
+roms=$(cat links | cut -d "/" -f5 | cut -d '"' -f1)
+for link in $(echo $roms); do
+wget --progress=bar $site$link
+./create_flashable_firmware.sh $link
+rm $link; done
+echo Uploading:
 for file in *.zip; do wput $file ftp://$afhuser:$afhpass@uploads.androidfilehost.com//mifirmware/$miuidate/ ; done
 for file in *.zip; do wput $file ftp://$basketbuilduser:$basketbuildpass@basketbuild.com//mifirmware/$miuidate/ ; done
