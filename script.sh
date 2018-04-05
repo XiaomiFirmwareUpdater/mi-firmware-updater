bb289c06dfb0ba00484d6baeb3ef8184fd86da34
yshalsager <ysh-alsager@hotmail.com>
2018-04-05 22:51:12 +0200

    V2.5

--- script.sh
+++ script.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo Exporting variables:
-miuidate=8.3.22 && echo "Latest miui update is $miuidate"
+miuidate=$(curl -s http://en.miui.com/forum.php | grep http://en.miui.com/download.html | grep -o '[0-9]*[.][0-9]*[.][0-9]*') && echo "Latest miui update is $miuidate"
+if [ "$miuidate" == "$(< 	)" ]; then
+echo "No new updates!" ; exit
+else
+echo $miuidate > miuiversion
+fi
 site=http://bigota.d.miui.com/$miuidate/
 echo Fetching updates:
 while read device; do
@@ -8,10 +13,11 @@ id=$(echo $device | cut -d , -f1)
 name=$(echo $device | cut -d , -f2)
 {
 echo Device: $(cut -d _ -f1 <<< $name) ; echo id: $id
-curl -s http://en.miui.com/download-$id.html | grep miui_$name$miuidate | cut -d '"' -f6 | cut -d '"' -f1
+curl -s http://en.miui.com/download-$id.html | grep -Po '(?<=href=")[^"]*' | grep miui_$name$miuidate | grep zip
 } >> data
 done <devices
 cat data | grep -E "(http|https)://[a-zA-Z0-9./?=-]*" > links
+mkdir -p changelog/$miuidate/
 echo Starting:
 wget -qq --progress=bar https://github.com/xiaomi-firmware-updater/xiaomi-flashable-firmware-creator/raw/master/create_flashable_firmware.sh && chmod +x create_flashable_firmware.sh
 roms=$(cat links | cut -d "/" -f5 | cut -d '"' -f1)
@@ -28,3 +34,6 @@ cd Global; for file in *.zip; do product=$(echo $file | cut -d _ -f3); wput $fil
 for file in *.zip; do product=$(echo $file | cut -d _ -f3); wput $file ftp://$afhuser:$afhpass@uploads.androidfilehost.com//Xiaomi-Firmware/Developer/$miuidate/Global/$product/ ; done
 cd ../China; for file in *.zip; do product=$(echo $file | cut -d _ -f3); wput $file ftp://$basketbuilduser:$basketbuildpass@basketbuild.com//Xiaomi-Firmware/Developer/$miuidate/China/$product/ ; done
 for file in *.zip; do product=$(echo $file | cut -d _ -f3); wput $file ftp://$afhuser:$afhpass@uploads.androidfilehost.com//Xiaomi-Firmware/Developer/$miuidate/China/$product/ ; done
+echo Pushing:
+git add miuiversion changelog/ ; git commit -m "$miuidate"
+git git remote set-url origin https://$gituser:$gitpass@github.com/xiaomi-firmware-updater/mi-firmware-updater.git ; git push origin testing
