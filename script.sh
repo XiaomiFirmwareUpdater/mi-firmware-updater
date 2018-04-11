130903b820439af945ed2417b2b32166d730b6e6
yshalsager <ysh-alsager@hotmail.com>
2018-04-11 21:26:50 +0200

    V2.5 update
    Changes:
    - Add check before start, will cancel the build if MIUI still not updated.
    - Changed file naming system in server to match device codename instead of MIUI zip
    - Introducing a changelog feature "by comparing "NON-HLOS.bin/versioninfo/verinfo/ver_info.txt"
    - Add md5 hash

--- script.sh
+++ script.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo Exporting variables:
 miuidate=$(curl -s http://en.miui.com/forum.php | grep http://en.miui.com/download.html | grep -o '[0-9]*[.][0-9]*[.][0-9]*') && echo "Latest miui update is $miuidate"
+if [ "$miuidate" == "$(< miuiversions | head -n1)" ]; then
+echo "No new updates!" ; exit
+else
+sed -i "1i $miuidate" miuiversions
+fi
 site=http://bigota.d.miui.com/$miuidate/
 echo Fetching updates:
 while read device; do
@@ -8,24 +13,30 @@ id=$(echo $device | cut -d , -f1)
 name=$(echo $device | cut -d , -f2)
 {
 echo Device: $(cut -d _ -f1 <<< $name) ; echo id: $id
-curl -s http://en.miui.com/download-$id.html | grep miui_$name$miuidate | cut -d '"' -f6 | cut -d '"' -f1
+curl -s http://en.miui.com/download-$id.html | grep -Po '(?<=href=")[^"]*' | grep miui_$name$miuidate | grep zip
 } >> data
 done <devices
 cat data | grep -E "(http|https)://[a-zA-Z0-9./?=-]*" > links
+mkdir -p changelog/$miuidate/
 echo Starting:
-wget -qq --progress=bar https://github.com/xiaomi-firmware-updater/xiaomi-flashable-firmware-creator/raw/master/create_flashable_firmware.sh && chmod +x create_flashable_firmware.sh
+wget -qq --progress=bar https://github.com/xiaomi-firmware-updater/xiaomi-flashable-firmware-creator/raw/next/create_flashable_firmware.sh && chmod +x create_flashable_firmware.sh
 roms=$(cat links | cut -d "/" -f5 | cut -d '"' -f1)
 for link in $(echo $roms); do
 echo Downloading $link; wget -qq --progress=bar $site$link
 ./create_flashable_firmware.sh $link
 rm $link; done
+md5sum *.zip > changelog/$miuidate/$miuidate.md5
 find . -type f -size 0b -delete
 echo Moving Files:
 mkdir Global; mkdir China
 for zip in `find -name "*Global*"`; do mv $zip Global/; done
 for zip in *.zip; do mv $zip China/; done
 echo Uploading:
-cd Global; for file in *.zip; do product=$(echo $file | cut -d _ -f3); wput $file ftp://$basketbuilduser:$basketbuildpass@basketbuild.com//Xiaomi-Firmware/Developer/$miuidate/Global/$product/ ; done
-for file in *.zip; do product=$(echo $file | cut -d _ -f3); wput $file ftp://$afhuser:$afhpass@uploads.androidfilehost.com//Xiaomi-Firmware/Developer/$miuidate/Global/$product/ ; done
-cd ../China; for file in *.zip; do product=$(echo $file | cut -d _ -f3); wput $file ftp://$basketbuilduser:$basketbuildpass@basketbuild.com//Xiaomi-Firmware/Developer/$miuidate/China/$product/ ; done
-for file in *.zip; do product=$(echo $file | cut -d _ -f3); wput $file ftp://$afhuser:$afhpass@uploads.androidfilehost.com//Xiaomi-Firmware/Developer/$miuidate/China/$product/ ; done
+cd Global; for file in *.zip; do product=$(echo $file | cut -d _ -f2); wput $file ftp://$basketbuilduser:$basketbuildpass@basketbuild.com//Xiaomi-Firmware/Developer/$miuidate/Global/$product/ ; done
+for file in *.zip; do product=$(echo $file | cut -d _ -f2); wput $file ftp://$afhuser:$afhpass@uploads.androidfilehost.com//Xiaomi-Firmware/Developer/$miuidate/Global/$product/ ; done
+cd ../China; for file in *.zip; do product=$(echo $file | cut -d _ -f2); wput $file ftp://$basketbuilduser:$basketbuildpass@basketbuild.com//Xiaomi-Firmware/Developer/$miuidate/China/$product/ ; done
+for file in *.zip; do product=$(echo $file | cut -d _ -f2); wput $file ftp://$afhuser:$afhpass@uploads.androidfilehost.com//Xiaomi-Firmware/Developer/$miuidate/China/$product/ ; done
+echo Pushing:
+cd .. ; git config --global user.email "$gitmail" ; git config --global user.name "$gituser"
+git add miuiversions changelog/ ; git commit -m "$miuidate"
+git push -q https://$GIT_OAUTH_TOKEN_XFU@github.com/xiaomi-firmware-updater/mi-firmware-updater.git weekly
