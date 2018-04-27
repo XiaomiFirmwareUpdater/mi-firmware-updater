21b3189b6f03e26f32f3f026eca59553df146b4c
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2018-04-27 16:50:05 +0200

    better fetch method

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-function getmiuidate() { 
+function getmiuidate() {
 y=$(date '+%y' | cut -c2); month=$(date '+%m');
 tmpday=$(date '+%u')
 if [ $tmpday  == 4 ] ; then
@@ -45,57 +45,13 @@ echo Fetching updates:
 cat device | while read device; do
 id=$(echo $device | cut -d , -f1)
 name=$(echo $device | cut -d , -f2)
-sp=$(echo $device | cut -d , -f3)
-check
+curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | grep miui_$name$miuidate | cut -d '"' -f6 >> data
 done
-}
-
-function check() {
-if [ "$sp" = "" ]; then
-  if [[ $name = *"Global"* ]]; then
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | head -n2 | tail -n1 >> data
-  else
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | tail -n1 >> data
-  fi
-else
-  if [[ $sp = *"China"* ]]; then
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | tail -n1 >> data
-  elif [[ $sp = *"hydrogen_g"* ]]; then
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | head -n2 | tail -n1 >> data
-  elif [[ $sp = *"hydrogen_c"* ]]; then
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | head -n6 | tail -n1 >> data
-  elif [[ $sp = *"helium_g"* ]]; then
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | head -n4 | tail -n1 >> data
-  elif [[ $sp = *"helium_c"* ]]; then
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | head -n8 | tail -n1 >> data
-  elif [[ $sp = *"kate"* ]]; then
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | head -n6 | tail -n1 >> data
-  elif [[ $sp = *"kenzo_g"* ]]; then
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | head -n2 | tail -n1 >> data
-  elif [[ $sp = *"kenzo_c"* ]]; then
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | head -n4 | tail -n1 >> data
-  elif [[ $sp = *"mido"* ]]; then
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | head -n4 | tail -n1 >> data
-  elif [[ $sp = *"ugg_g"* ]]; then
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | head -n2 | tail -n1 >> data
-  elif [[ $sp = *"ugg_c"* ]]; then
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | head -n6 | tail -n1 >> data
-  elif [[ $sp = *"ugglite_g"* ]]; then
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | head -n4 | tail -n1 >> data
-  elif [[ $sp = *"ugglite_c"* ]]; then
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | head -n8 | tail -n1 >> data
-  elif [[ $sp = *"vince_g"* ]]; then
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | head -n2 | tail -n1 >> data
-  elif [[ $sp = *"vince_c"* ]]; then
-  curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | cut -d '"' -f6 | head -n7 | tail -n1 >> data
-  fi
-fi
 }
 
 function download_extract() {
 wget -qq --progress=bar https://github.com/xiaomi-firmware-updater/xiaomi-flashable-firmware-creator/raw/master/create_flashable_firmware.sh && chmod +x create_flashable_firmware.sh
-cat data | grep '$miuidate' >> links
-cat links | while read link; do
+cat data | while read link; do
 zip=$(echo $link | cut -d "/" -f5 | cut -d '"' -f1)
 echo Downloading $zip
 wget -qq --progress=bar $link
