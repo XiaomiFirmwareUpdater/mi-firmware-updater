e60b82e623c66c05184043939d2c7ab0c20af6a2
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2018-05-02 11:45:29 +0200

    Stable V3

--- script.sh
+++ script.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
+function getmiuiversion() {
 echo Exporting variables:
 miuiver="V$(curl -s http://en.miui.com/download.html | grep version | grep -o 'MIUI[0-9]*' | cut -c 5 | awk 'NR==2')" && echo "Latest miui update is $miuiver"
+}
 
-site=http://bigota.d.miui.com/$miuiver/
+function fetch() {
 echo Fetching updates:
-while read device; do
+cat device | while read device; do
 id=$(echo $device | cut -d , -f1)
 name=$(echo $device | cut -d , -f2)
-{
 echo Device: $(cut -d _ -f1 <<< $name) ; echo id: $id
-curl -s http://en.miui.com/download-$id.html | grep miui_$name$miuiver | cut -d '"' -f 6 | head -n1
-} >> data
-done <devices
-cat data | grep -E "(http|https)://[a-zA-Z0-9./?=-]*" > links
-echo Starting:
+curl -s http://en.miui.com/download-$id.html | grep 'margin-top: 0' | grep miui_$name$miuiver | cut -d '"' -f6 | head -n1 >> data
+done
+}
+
+function download_extract() {
 wget -qq --progress=bar https://github.com/xiaomi-firmware-updater/xiaomi-flashable-firmware-creator/raw/master/create_flashable_firmware.sh && chmod +x create_flashable_firmware.sh
-roms=$(cat links)
-for link in $(echo $roms); do
-file=$(echo $link | cut -d "/" -f5 | cut -d '"' -f1)
-echo Downloading $file; wget -qq --progress=bar $link
-./create_flashable_firmware.sh $file
-rm $file; done
-echo Moving Files:
-mkdir Global; mkdir China
-for zip in `find -name "*Global*"`; do mv $zip Global/; done
-for zip in *.zip; do mv $zip China/; done
-#Uploading
-cd Global; for file in *.zip; do product=$(echo $file | cut -d _ -f2); wput $file ftp://$basketbuilduser:$basketbuildpass@basketbuild.com//Xiaomi-Firmware/Stable/$miuiver/Global/$product/ ; done
-for file in *.zip; do product=$(echo $file | cut -d _ -f2); wput $file ftp://$afhuser:$afhpass@uploads.androidfilehost.com//Xiaomi-Firmware/Stable/$miuiver/Global/$product/ ; done
-cd ../China; for file in *.zip; do product=$(echo $file | cut -d _ -f2); wput $file ftp://$basketbuilduser:$basketbuildpass@basketbuild.com//Xiaomi-Firmware/Stable/$miuiver/China/$product/ ; done
-for file in *.zip; do product=$(echo $file | cut -d _ -f2); wput $file ftp://$afhuser:$afhpass@uploads.androidfilehost.com//Xiaomi-Firmware/Stable/$miuiver/China/$product/ ; done
+cat data | while read link; do
+zip=$(echo $link | cut -d "/" -f5 | cut -d '"' -f1)
+echo Downloading $zip
+wget -qq --progress=bar $link
+./create_flashable_firmware.sh $zip
+rm $zip; done
+md5sum *.zip > changelog/$miuidate/$miuidate.md5
+find . -type f -size 0k -delete
+}
+
+function upload() {
+mkdir -p ~/.ssh  &&  echo "Host *" > ~/.ssh/config && echo " StrictHostKeyChecking no" >> ~/.ssh/config
+echo Uploading Files:
+for file in *.zip; do product=$(echo $file | cut -d _ -f2); sshpass -p $sfpass rsync -avP -e ssh $file yshalsager@web.sourceforge.net:/home/frs/project/xiaomi-firmware-updater/Stable/$miuiver/$product/ ; done
+for file in *.zip; do product=$(echo $file | cut -d _ -f2); wput $file ftp://$afhuser:$afhpass@uploads.androidfilehost.com//Xiaomi-Firmware/Stable/$miuiver/$product/ ; done
+}
+
+function push() {
+echo Pushing:
+git config --global user.email "$gitmail" ; git config --global user.name "$gituser"
+git add miuiversion changelog/ ; git commit -m "$miuidate"
+git push -q https://$GIT_OAUTH_TOKEN_XFU@github.com/XiaomiFirmwareUpdater/$repo.git $branch
+}
+
+# Start
+getmiuiversion
+fetch
+mkdir -p changelog/$miuidate/
+download_extract
+upload
+push
