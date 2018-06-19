edb145a11832814576857d1c92055c1e58a6d967
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2018-06-19 23:14:52 +0200

    stable: add missing lines

--- script.sh
+++ script.sh
@@ -6,6 +6,8 @@ android=$(echo $device | cut -d , -f2)
 id=$(echo $device | cut -d , -f3)
 url=`./getversion.sh $codename F $android`
 miuiver=$(echo $url | cut -d / -f4 | cut -d _ -f3 | cut -d . -f1)
+echo $url >> data
+done
 
 wget -qq --progress=bar https://github.com/XiaomiFirmwareUpdater/xiaomi-flashable-firmware-creator/raw/master/create_flashable_firmware.sh && chmod +x create_flashable_firmware.sh
 cat data | while read link; do
@@ -19,4 +21,4 @@ mkdir -p ~/.ssh  &&  echo "Host *" > ~/.ssh/config && echo " StrictHostKeyChecki
 echo Uploading Files:
 for file in *.zip; do product=$(echo $file | cut -d _ -f2); sshpass -p $sfpass rsync -avP -e ssh $file yshalsager@web.sourceforge.net:/home/frs/project/xiaomi-firmware-updater/Stable/$miuiver/$product/ ; done
 for file in *.zip; do product=$(echo $file | cut -d _ -f2); wput $file ftp://$afhuser:$afhpass@uploads.androidfilehost.com//Xiaomi-Firmware/Stable/$miuiver/$product/ ; done
-for file in *.zip; do product=$(echo $file | cut -d _ -f2); wput $file ftp://$basketbuilduser:$basketbuildpass@basketbuild.com//Xiaomi-Firmware/Stable/$miuiver/$product/ ; done
+#for file in *.zip; do product=$(echo $file | cut -d _ -f2); wput $file ftp://$basketbuilduser:$basketbuildpass@basketbuild.com//Xiaomi-Firmware/Stable/$miuiver/$product/ ; done
