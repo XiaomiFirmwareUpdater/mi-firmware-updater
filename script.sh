4877f39039008d42061afdaced9fcfb58ec7c70b
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2018-07-22 17:38:51 +0200

    script: telegram: use tag link for GitHub downloads

--- script.sh
+++ script.sh
@@ -100,7 +100,7 @@ for file in *.zip; do
 	*Filesize*: $size
 	*MD5*: $md5
 	*Download Links*:
-	[Sourceforge](https://sourceforge.net/projects/xiaomi-firmware-updater/files/Developer/$version/) - [Github](https://github.com/XiaomiFirmwareUpdater/firmware_xiaomi_$codename/releases/latest)
+	[Sourceforge](https://sourceforge.net/projects/xiaomi-firmware-updater/files/Developer/$version/) - [Github](https://github.com/XiaomiFirmwareUpdater/firmware_xiaomi_$codename/releases/tag/$GIT_TAG)
 	@XiaomiFirmwareUpdater | @MIUIUpdatesTracker"
 done
 else
