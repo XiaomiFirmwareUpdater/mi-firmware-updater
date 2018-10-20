153f40a9dc32098cd107d8fd0cd7f32cc2d4fa41
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2018-10-20 21:51:35 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+check_db () {
 #Check if db exist
 if [ -e miuiversion ]
 then
@@ -6,10 +8,14 @@ then
 else
     echo "DB not found!"
 fi
+}
 
+get_script () {
 #Download
 curl -H "PRIVATE-TOKEN: $GITLAB_OAUTH_TOKEN_VE" 'https://gitlab.com/api/v4/projects/7746867/repository/files/getversion.sh/raw?ref=master' -o getversion.sh && chmod +x getversion.sh
+}
 
+fetch_updates () {
 #Fetch
 echo Fetching updates:
 cat device | while read device; do
@@ -22,7 +28,9 @@ echo $tmpname"="$url >> raw_out
 sed -i 's/param error/not avilable/g' ./raw_out
 done
 cat raw_out | sort | sed 's/http.*miui_//' | cut -d _ -f1,2 | sed 's/-/_/g' > miuiversion
+}
 
+compare () {
 #Compare
 echo Comparing:
 cat miuiversion | while read rom; do
@@ -32,7 +40,9 @@ cat miuiversion | while read rom; do
 	diff <(echo "$old") <(echo "$new") | grep ^"<\|>" >> compare
 done
 awk '!seen[$0]++' compare > changes
+}
 
+check_changes () {
 #Info
 if [ -s changes ]
 then
@@ -41,7 +51,9 @@ then
 else
     echo "No changes found!"
 fi
+}
 
+get_links () {
 #Downloads
 if [ -s updates ]
 then
@@ -50,7 +62,9 @@ then
 else
     echo "No new updates!"
 fi
+}
 
+check_exist () {
 #Check if this is a rolled out update
 cat updates | while read update; do
 git grep -q "$update" $(git rev-list --all) -- miuiversion
@@ -62,10 +76,11 @@ else
     echo "$update" - "Seems this is a new update."
 fi
 done
+}
 
+main () {
 #Start
-if [ -s dl_links ]
-then
+
 wget -qq --progress=bar https://github.com/XiaomiFirmwareUpdater/xiaomi-flashable-firmware-creator/raw/master/create_flashable_firmware.sh && chmod +x create_flashable_firmware.sh
 cat dl_links | while read link; do
 dl=$(echo $link | cut -d = -f2)
@@ -76,7 +91,9 @@ aria2c -x16 -q $dl
 mkdir -p changelog/$ver/
 ./create_flashable_firmware.sh $zip
 rm $zip; done
+}
 
+generate_diff () {
 #Diff
 for file in *.zip; do version=$(echo $file | cut -d _ -f5)
 if [[ $file =~ ^[a-z]*_[a-z]*_[a-z]*_[A-Z0-9]*_V[0-9]*.[0-9]*.[0-9]*.[0-9]*.[A-Z][A-Z][A-Z]MI[A-Z][A-Z]_[a-z0-9]*_[0-9]*.[0-9]*.[a-z]*$ ]]; then
@@ -91,7 +108,9 @@ else
 fi
 diff changelog/$oldversion/*.log changelog/$version/*.log > changelog/$version/$oldversion-$version.diff
 done
+}
 
+upload () {
 #Upload
 echo Uploading Files:
 mkdir -p ~/.ssh  &&  echo "Host *" > ~/.ssh/config && echo " StrictHostKeyChecking no" >> ~/.ssh/config
@@ -103,7 +122,9 @@ quit
 EOF
 sshpass -p $sfpass rsync -avP -e ssh $file yshalsager@web.sourceforge.net:/home/frs/project/xiaomi-firmware-updater/Developer/$version/$product/ ; done
 for file in *.zip; do product=$(echo $file | cut -d _ -f2); version=$(echo $file | cut -d _ -f5); wput $file ftp://$afhuser:$afhpass@uploads.androidfilehost.com//Xiaomi-Firmware/Developer/$version/$product/ ; done
+}
 
+push () {
 #Push
 echo Pushing:
 git add miuiversion changelog/ ; git -c "user.name=$gituser" -c "user.email=$gitmail" commit -m "Sync: $(date +%d.%m.%Y)"
@@ -111,7 +132,9 @@ export GIT_TAG=$branch-$(date +%d.%m.%Y)
 github-release release -u XiaomiFirmwareUpdater -r $repo -t $GIT_TAG -n "$GIT_TAG" -d "Sync: $(date +%d.%m.%Y), upload firmware from $(cat updates | tr '\n' '&') MIUI ROM"
 git push -q https://$GIT_OAUTH_TOKEN_XFU@github.com/XiaomiFirmwareUpdater/$repo.git HEAD:$branch
 for file in *.zip; do name=$(echo $file); github-release upload -u XiaomiFirmwareUpdater -r $repo -t $GIT_TAG -n "$name" -f $name; done
+}
 
+telegram () {
 #Telegram
 wget -q https://github.com/yshalsager/telegram.sh/raw/master/telegram && chmod +x telegram
 for file in *.zip; do 
@@ -133,9 +156,28 @@ for file in *.zip; do
 	[Sourceforge](https://sourceforge.net/projects/xiaomi-firmware-updater/files/Developer/$version/) - [Github](https://github.com/XiaomiFirmwareUpdater/firmware_xiaomi_$codename/releases/tag/$GIT_TAG)
 	@XiaomiFirmwareUpdater | @MIUIUpdatesTracker"
 done
+}
+
+clean () {
+#Cleanup
+rm raw_out compare changes updates dl_links 2> /dev/null
+}
+
+# run
+check_db
+get_script
+compare
+check_changes
+get_links
+check_exist
+if [ -s dl_links ]
+then
+main
+#generate_diff
+upload
+push
+telegram
+clean
 else
     echo "Nothing found!" && exit 0
 fi
-
-#Cleanup
-rm raw_out compare changes updates dl_links 2> /dev/null
