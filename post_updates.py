c38a4e39083b8a314238c4f79ff9532bbf920de4
yshalsager <ysh-alsager@hotmail.com>
2020-03-13 22:58:06 +0200

    post_updates: refactor and cleanup
    * Add XDA poster, well implemented this time.
    * organize code into classes
    
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- post_updates.py
+++ post_updates.py
@@ -2,22 +2,117 @@
 # pylint: disable=too-many-locals
 
 from os import environ, stat
+from time import sleep
 
-# import json
+import yaml
+from requests import get
 from telegram import Bot
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-# from requests import get, post
 from telegram.ext import Updater
-
 from helpers import set_region, get_device_name
+from xda_poster.xda import XDA
 
 BOT_TOKEN = environ['bottoken']
 TG_CHAT = "@XiaomiFirmwareUpdater"
-# XDA_API_KEY = environ['XDA_TOKEN']
+XDA_USERNAME = environ['XDA_USERNAME']
+XDA_PASSWORD = environ['XDA_PASSWORD']
+
 BOT = Bot(token=BOT_TOKEN)
 UPDATER = Updater(bot=BOT, use_context=True)
 
 SITE = 'https://xiaomifirmwareupdater.com'
+
+
+class Message:
+    """Firmware update message
+    :param :names devices names dictionary
+    :param :data XiaomiFirmwareUpdater log line"""
+
+    def __init__(self, data, names):
+        self.names = names
+        self.data = data
+        self.info = self.get_info()
+
+    def get_codename(self):
+        """Returns the codename from log line"""
+        return self.data.split('|')[3]
+
+    def get_info(self):
+        """Returns update info dictionary"""
+        data = self.data.split('|')
+        process = data[0]
+        branch = "Stable" if data[1] == "stable" else "Developer"
+        codename = data[3]
+        version = data[4]
+        link = None
+        if process == 'firmware':
+            link = f'{SITE}/firmware/{codename}/'
+        elif process == 'non-arb firmware':
+            if 'V' in version:
+                version = version.split('.')[0]
+            link = f'https://osdn.net/projects/xiaomifirmwareupdater/' \
+                   f'storage/non-arb/{branch}/{version}/{codename}/'
+        info = {
+            "process": process,
+            "branch": branch,
+            "device": data[2],
+            "codename": codename,
+            "version": version,
+            "android": data[5],
+            "zip_name": data[6],
+            "zip_size": data[7],
+            "md5_hash": data[8],
+            "region": set_region(data[7]),
+            "name": get_device_name(data[2], self.names),
+            "link": link
+        }
+        return info
+
+    def generate_telegram_message(self):
+        """Generates a Telegram message from update info"""
+        telegram_message = f"*New {self.info['branch']} {self.info['process']} " \
+                           f"update available!*\n" \
+                           f"_Device:_ {self.info['name']} (#{self.info['codename']})\n" \
+                           f"_Size:_ {self.info['zip_size']}\n" \
+                           f"_MD5:_ `{self.info['md5_hash']}`"
+        download = InlineKeyboardButton(f"{self.info['version']} | {self.info['android']}",
+                                        f"{self.info['link']}")
+        archive = InlineKeyboardButton(f"Firmware Archive",
+                                       f"{SITE}/archive/firmware/{self.info['codename']}/")
+        xfu_channel = InlineKeyboardButton("XiaomiFirmwareUpdater",
+                                           url="https://t.me/XiaomiFirmwareUpdater")
+        mut_channel = InlineKeyboardButton("MIUIUpdatesTracker",
+                                           url="https://t.me/MIUIUpdatesTracker")
+        reply_markup = InlineKeyboardMarkup(
+            [[download, archive], [xfu_channel, mut_channel]]
+        )
+        return telegram_message, reply_markup
+
+    def generate_xda_message(self, xda_template):
+        """Generates XDA message from update info"""
+        return xda_template.replace('$branch', self.info['branch']) \
+            .replace('$process', self.info['process'].capitalize()) \
+            .replace('$version', self.info['version']).replace('$android', self.info['android']) \
+            .replace('$region', self.info['region']) \
+            .replace('$name', self.info['zip_name']).replace('$zip_size', self.info['zip_size']) \
+            .replace('$md5_hash', self.info['md5_hash']) \
+            .replace('$link', self.info['link']).replace('$codename', self.info['codename'])
+
+
+class XDAPoster(XDA):
+    """XDA API client"""
+    def __init__(self):
+        with open('xda_threads.yml', 'r') as file:
+            self.threads = yaml.load(file, Loader=yaml.CLoader)
+        with open('xda_template.txt', 'r') as template:
+            self.template = template.read()
+        super().__init__()
+
+    def get_post_id(self, codename):
+        """Get post ID from xda thread"""
+        return get("https://api.xda-developers.com/v3/posts",
+                   params={"threadid": self.threads[codename]}
+                   ).json()["results"][0]["postid"]
 
 
 def post_updates(names):
@@ -26,90 +121,22 @@ def post_updates(names):
     """
     if stat('log').st_size == 0:
         return
-    # with open('xda_threads.yml', 'r') as file:
-    #     xda_threads = yaml.load(file, Loader=yaml.CLoader)
-    # with open('xda_template.txt', 'r') as template:
-    #     xda_template = template.read()
     with open('log', 'r') as log:
-        for line in log:
-            info = line.split('|')
-            process = info[0]
-            if info[1] == 'stable':
-                branch = 'Stable'
-            elif info[1] == 'weekly':
-                branch = 'Developer'
-            device = info[2]
-            codename = info[3]
-            version = info[4]
-            android = info[5]
-            # zip_name = info[6]
-            zip_size = info[7]
-            md5_hash = info[8]
-            # region = set_region(zip_name)
-            version_ = ""
-            if process == 'firmware':
-                link = f'{SITE}/firmware/{codename}/'
-            elif process == 'non-arb firmware':
-                if 'V' in version:
-                    version_ = version.split('.')[0]
-                else:
-                    version_ = version
-                link = f'https://osdn.net/projects/xiaomifirmwareupdater/' \
-                       f'storage/non-arb/{branch}/{version_}/{codename}/'
-            name = get_device_name(device, names)
-            # post to tg
-            telegram_message = f"*New {branch} {process} update available!*\n" \
-                               f"_Device:_ {name} (#{codename})\n" \
-                               f"_Size:_ {zip_size}\n" \
-                               f"_MD5:_ `{md5_hash}`"
-            download = InlineKeyboardButton(f"{version} | {android}",
-                                            f"{link}")
-            archive = InlineKeyboardButton(f"Firmware Archive",
-                                           f"{SITE}/archive/firmware/{codename}/")
-            xfu_channel = InlineKeyboardButton("XiaomiFirmwareUpdater",
-                                               url="https://t.me/XiaomiFirmwareUpdater")
-            mut_channel = InlineKeyboardButton("MIUIUpdatesTracker",
-                                               url="https://t.me/MIUIUpdatesTracker")
-            reply_markup = InlineKeyboardMarkup(
-                [[download, archive], [xfu_channel, mut_channel]]
-            )
-            UPDATER.bot.send_message(chat_id=TG_CHAT, text=telegram_message,
-                                     parse_mode='Markdown', disable_web_page_preview='yes',
-                                     reply_markup=reply_markup)
-            # post to XDA
-            # if codename not in xda_threads.keys():
-            #     continue
-            # try:
-            #     xda_post_id = get("https://api.xda-developers.com/v3/posts",
-            #                       params={"threadid": xda_threads[codename]}
-            #                       ).json()["results"][0]["postid"]
-            # except (KeyError, IndexError):
-            #     continue
-            # xda_post = xda_template.replace('$branch', branch) \
-            #     .replace('$process', process.capitalize()) \
-            #     .replace('$version', version).replace('$android', android) \
-            #     .replace('$region', region) \
-            #     .replace('$name', name).replace('$zip_size', zip_size) \
-            #     .replace('$md5_hash', md5_hash) \
-            #     .replace('$link', link).replace('$codename', codename)
-            # data = {"postid": xda_post_id, "message": xda_post}
-            # headers = {
-            #     'origin': 'https://api.xda-developers.com',
-            #     'accept-encoding': 'gzip, deflate, br',
-            #     'accept-language': 'en-US,en;q=0.9',
-            #     'authorization': 'Bearer ' + XDA_API_KEY,
-            #     'x-requested-with': 'XMLHttpRequest',
-            #     'content-type': 'application/json',
-            #     'accept': 'application/json, text/javascript, */*; q=0.01',
-            #     'referer': 'https://api.xda-developers.com/explorer/',
-            #     'authority': 'api.xda-developers.com',
-            #     'sec-fetch-site': 'same-origin',
-            # }
-            # xda_req = post('https://api.xda-developers.com/v3/posts/new',
-            #                data=json.dumps(data), headers=headers)
-            # if xda_req.status_code == 200:
-            #     print(f"{device}: XDA post created successfully")
-            #     sleep(15)
-            # else:
-            #     print("XDA Error")
-            #     print(xda_req.reason)
+        updates = log.readlines()
+    xda = XDAPoster()
+    for update in updates:
+        message = Message(update, names)
+        # post to tg
+        telegram_message, reply_markup = message.generate_telegram_message()
+        UPDATER.bot.send_message(chat_id=TG_CHAT, text=telegram_message,
+                                 parse_mode='Markdown', disable_web_page_preview='yes',
+                                 reply_markup=reply_markup)
+        sleep(1)
+        # post to XDA
+        codename = message.get_codename()
+        if codename not in xda.threads.keys():
+            continue
+        xda_post_id = xda.get_post_id(codename)
+        xda_post = message.generate_xda_message(xda.template)
+        xda.post(xda_post_id, xda_post)
+        sleep(15)
